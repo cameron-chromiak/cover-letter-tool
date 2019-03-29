36c12456cc0a7796641787319344d1f5c9f2e1b1
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-29 15:29:21 -0400

    worked on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,12 +15,13 @@ class FormField extends Component{
 
   render(){
     return(
-      <div>
-        <form className='ui form'>
-        <h4 className="ui dividing header">Shipping Information</h4>
-          <div className="field form-container">
-            <div className="field four wide">
-              <input type="text" name="info[first-name]" placeholder="First Name"/>
+    <div>
+
+      <form className='ui form form-container'>
+        <div className="field">
+          <div className="field four wide">
+            <h5>Recipient Info</h5>
+            <input type="text" name="info[first-name]" placeholder="First Name"/>
           </div>
           <div className="field four wide">
               <input type="text" name="info[last-name]" placeholder="Last Name"/>
@@ -35,8 +36,82 @@ class FormField extends Component{
               <input type="text" name="info[company-name]" placeholder="Last Name"/>
           </div>
         </div>
-      </form>
+
+    <div class="field">
+        <div class="four wide field">
+          <h4>Company Info</h4>
+          <input type="text" name="company[title]" placeholder="Street Address"/>
+        </div>
+        <div class="four wide field">
+          <input type="text" name="company[address]" placeholder="Apt #"/>
+        </div>
     </div>
+
+     <div className="fields">
+     <div className='two wide field'>
+       <h4>State</h4>
+      <select className="ui fluid dropdown">
+       <option value="">State</option>
+         <option value="AL">Alabama</option>
+         <option value="AK">Alaska</option>
+         <option value="AZ">Arizona</option>
+         <option value="AR">Arkansas</option>
+         <option value="CA">California</option>
+         <option value="CO">Colorado</option>
+         <option value="CT">Connecticut</option>
+         <option value="DE">Delaware</option>
+         <option value="DC">District Of Columbia</option>
+         <option value="FL">Florida</option>
+         <option value="GA">Georgia</option>
+         <option value="HI">Hawaii</option>
+         <option value="ID">Idaho</option>
+         <option value="IL">Illinois</option>
+         <option value="IN">Indiana</option>
+         <option value="IA">Iowa</option>
+         <option value="KS">Kansas</option>
+         <option value="KY">Kentucky</option>
+         <option value="LA">Louisiana</option>
+         <option value="ME">Maine</option>
+         <option value="MD">Maryland</option>
+         <option value="MA">Massachusetts</option>
+         <option value="MI">Michigan</option>
+         <option value="MN">Minnesota</option>
+         <option value="MS">Mississippi</option>
+         <option value="MO">Missouri</option>
+         <option value="MT">Montana</option>
+         <option value="NE">Nebraska</option>
+         <option value="NV">Nevada</option>
+         <option value="NH">New Hampshire</option>
+         <option value="NJ">New Jersey</option>
+         <option value="NM">New Mexico</option>
+         <option value="NY">New York</option>
+         <option value="NC">North Carolina</option>
+         <option value="ND">North Dakota</option>
+         <option value="OH">Ohio</option>
+         <option value="OK">Oklahoma</option>
+         <option value="OR">Oregon</option>
+         <option value="PA">Pennsylvania</option>
+         <option value="RI">Rhode Island</option>
+         <option value="SC">South Carolina</option>
+         <option value="SD">South Dakota</option>
+         <option value="TN">Tennessee</option>
+         <option value="TX">Texas</option>
+         <option value="UT">Utah</option>
+         <option value="VT">Vermont</option>
+         <option value="VA">Virginia</option>
+         <option value="WA">Washington</option>
+         <option value="WV">West Virginia</option>
+         <option value="WI">Wisconsin</option>
+         <option value="WY">Wyoming</option>
+        </select>
+      </div>
+        <div  className='field two wide'>
+          <h4>Zip Code</h4>
+          <input type="number" pattern="[0-9]{5}" maxLength="5" />
+        </div>
+    </div>
+    </form>
+  </div>
     )
   }
 
